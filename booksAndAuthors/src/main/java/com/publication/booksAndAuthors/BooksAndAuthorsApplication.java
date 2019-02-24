15696502a62ddaead9a7428c1e4f7bdddde7356d
Sunny Shekhar <35186060+beingCurious@users.noreply.github.com>
2019-02-24 16:41:03 +0530

    Initial Commit: Spring initializer

---
+++ booksAndAuthors/src/main/java/com/publication/booksAndAuthors/BooksAndAuthorsApplication.java
@@ -0,0 +1,13 @@
+package com.publication.booksAndAuthors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAndAuthorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAndAuthorsApplication.class, args);
+	}
+
+}
