0a7793d0bc91a4c6def5bbdff24ab04f3b13d3c0
Sunny Shekhar <35186060+beingCurious@users.noreply.github.com>
2019-03-02 22:20:20 +0530

    JPA Example: @ManyToMany For Book and Author

--- booksAndAuthors/src/main/java/com/publication/booksAndAuthors/model/Author.java
+++ booksAndAuthors/src/main/java/com/publication/booksAndAuthors/model/Author.java
@@ -19,7 +19,7 @@ public class Author {
 	private String lastName;
 
 	// Target Side
-	@ManyToMany(mappedBy="authorsOfThisBook")
+	@ManyToMany(mappedBy = "authorsOfThisBook")
 	private Set<Book> booksByAuthor = new HashSet<>();
 
 	public Author() {
@@ -34,6 +34,31 @@ public class Author {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.booksByAuthor = booksByAuthor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	public Long getId() {
--- booksAndAuthors/src/main/java/com/publication/booksAndAuthors/model/Book.java
+++ booksAndAuthors/src/main/java/com/publication/booksAndAuthors/model/Book.java
@@ -22,9 +22,8 @@ public class Book {
 	private String publisher;
 
 	@ManyToMany
-	@JoinTable(name="author_book", joinColumns=@JoinColumn(name="book_id"),
-	inverseJoinColumns=@JoinColumn(name="author_id")) 
-
+	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), 
+					inverseJoinColumns = @JoinColumn(name = "author_id"))
 	private Set<Author> authorsOfThisBook = new HashSet<>();
 
 	public Book() {
@@ -41,6 +40,31 @@ public class Book {
 		this.isbn = isbn;
 		this.publisher = publisher;
 		this.authorsOfThisBook = authorsOfThisBook;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	public Long getId() {
@@ -89,5 +113,4 @@ public class Book {
 				+ ", authorsOfThisBook=" + authorsOfThisBook + "]";
 	}
 
-	
 }
